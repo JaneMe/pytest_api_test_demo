db98b81c38f61c6203d2d31589ae86cd5f53d3f4
JaneMe <1301511732@qq.com>
2025-08-09 19:13:44 +0800

    feat：报告目录设置为隐藏目录 .report/ 并添加到 .gitignore 文件中忽略提交

--- run_allure_tests.sh
+++ run_allure_tests.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-# 一键生成并打开 Allure 报告
+
+# 目的：一键生成并打开 Allure 报告
 
 # 运行测试并生成 Allure 数据
 ENV=dev pytest tests 
 # 生成 Allure HTML 报告
-allure generate report/allure-results -o report/allure-report --clean
+allure generate .report/allure-results -o .report/allure-report --clean
 # 打开 Allure 报告的两种方式： server or open
-# allure server .allure-report 
-allure open report/allure-report 
+# allure server .report/allure-report 
+allure open .report/allure-report 
